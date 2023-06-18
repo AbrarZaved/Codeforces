f03a058cd9cf4a956c30dc171b45ddbca30ec671
AbrarZaved <abrarzaved2002@gmail.com>
2023-06-18 16:03:10 +0600

    new solve

---
+++ Codeforces(c++)/1772.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        char c;
+        cin>>a;
+        cin>>c;
+        cin>>b;
+        cout<<a+b<<endl;
+    }
+}
