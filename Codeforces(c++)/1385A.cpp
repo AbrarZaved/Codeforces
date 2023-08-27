81c016ea860d5ff876dadbd1347dc9de4e1942b9
AbrarZaved <abrarzaved2002@gmail.com>
2023-08-28 00:21:10 +0600

    new solve

--- Codeforces(c++)/1385A.cpp
+++ Codeforces(c++)/1385A.cpp
@@ -2,36 +2,17 @@
 #include <string>
 using namespace std;
 
-int main() {
-int t;
-cin>>t;
-while(t--){
-        std::string grid[8];
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        
+        
 
-        for (int row = 0; row < 8; ++row) {
-        std::cin >> grid[row];
+
+
     }
-
-    std::string word;
-    bool wordFound = false;
-
-    for (int col = 0; col < 8; ++col) {
-        word = "";
-        for (int row = 0; row < 8; ++row) {
-            if (grid[row][col] != '.') {
-                word += grid[row][col];
-            }
-        }
-
-        if (!word.empty()) {
-            std::cout << word << std::endl;
-            wordFound = true;
-            break;
-        }
-    }
-}
-
-  
-
-   
 }
---
+++ Codeforces(c++)/1385B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define hehe()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
+int main()
+{
+    hehe();
+    int t,n,a[110];
+    cin>>t;
+    while(t--){
+        cin>>n;
+        n *= 2;
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        int pos[110];
+        fill(pos,pos+110,0);
+
+        vector<int>v;
+        for(int i=n-1; i>=0; i--){
+            if(pos[a[i]]==0){
+                pos[a[i]]=1;
+                v.push_back(a[i]);
+            }
+        }
+        int s;
+        s=v.size();
+        for(int i=s-1; i>=0; i--)
+            cout<<v[i]<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
+
