87a95d3a5adecfae3e17b256c72737bacb70a29a
AbrarZaved <abrarzaved2002@gmail.com>
2023-08-30 15:27:51 +0600

    new solve

---
+++ Codeforces(c++)/1454A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll              long long
+#define pb              push_back
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+void print(vector<int> A,int n){
+     for(int i=0; i<n; i++){
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+}
+int main()
+{
+    fastread();
+    //freopen("input.txt","r", stdin);
+    ll t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<int>v;
+        for(int i=n; i>0; i--){
+            v.pb(i);
+        }
+        for(int i=0; i<n; i++){
+            if(v[i] == i+1){
+                swap(v[i],v[i+1]);
+            }
+        }
+        for(int i=0; i<n; i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
