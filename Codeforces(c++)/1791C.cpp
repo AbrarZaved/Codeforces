875799ea4193a58371d9e7c3e677fdf6e3933885
AbrarZaved <abrarzaved2002@gmail.com>
2023-10-05 22:19:43 +0600

    new solve

--- Codeforces(c++)/1791C.cpp
+++ Codeforces(c++)/1791C.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int a;
-    cin>>a;
-    while(a--){
-        string C;
-        //int x=0,y=0,count=0;
-        int b;
-        int d;
-        cin>>b;
-        cin>>C;
-        
-        //int count=0;
-        for(int i=0,j=b;i<b,j>0;i++,j--){
-            if((C[i] =='1' && C[j]=='0') || (C[i] =='0' && C[j]=='1'))
+    int t;
+    cin>>t;
+    while(t--){
+        string a;
+        int n,count=0;
+        cin>>n;
+        cin>>a;
+        int b=a.length();
+        for(int i=1;i<=n/2;i++){
+            if(a[i-1]=='1' && a[n-i]=='0')
             b=b-2;
-            //else
-            //d=d;
+            else if(a[i-1]=='0' && a[n-i]=='1')
+            b=b-2;
+            else
+            count=1;
 
-                       
-        }
+            if(count==1)
+            break;
+
+        }      
         printf("%d\n",b);
     }
 }
