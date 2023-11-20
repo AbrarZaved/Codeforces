4bb35309491d78dcd0fd20044a031485be387c61
AbrarZaved <abrarzaved2002@gmail.com>
2023-11-21 01:33:48 +0600

    new solve

---
+++ Codeforces(c++)/1790A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a[]={'3','1','4','1','5','9','2','6','5','3','5','8','9','7','9','3','2','3',
+        '8','4','6','2','6','4','3','3','8','3','2','7'};
+        string b;
+        cin>>b;
+        int c=b.length();
+        int count=0;
+        for(int i=0;i<c;i++){
+            if(b[i]==a[i])
+            ++count;
+            else
+            break;
+        }
+        cout<<count<<endl;
+    }
+}
