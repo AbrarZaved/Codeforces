6680b91e970393934cf4bbc768e5b9034c7b9aa6
AbrarZaved <abrarzaved2002@gmail.com>
2024-02-13 13:16:14 +0600

    new solve

---
+++ 1675A.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    a,b,c,x,y = map(int,input().split())
+        
+    if max(0,x-a)+max(0,y-b)<=c:
+        print("YES")
+    else:
+        print("NO")
+    
+        
+    
+    
