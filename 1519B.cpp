d1dff6cabe30e314450bfec6dbc6a63918d0b0db
AbrarZaved <abrarzaved2002@gmail.com>
2024-03-12 01:51:27 +0600

    new solve

---
+++ 1519B.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        int a[n][m],burels=0,i=1,j=1;
+        for(int b=1;b<=n;b++){
+            for(int l=1;l<=m;l++){
+                if (j!=m){
+                    j++;
+                    burels+=i;
+                }
+                else
+                    break;
+            }
+            if (i!=n){
+                i++;
+                burels+=j;
+            }
+            else
+                break;
+        }
+        if (burels==k)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+}
