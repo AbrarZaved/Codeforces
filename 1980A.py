2ab6c156d83b8abfb2abdfc01c6e95c511027cf9
AbrarZaved <abrarzaved2002@gmail.com>
2024-06-12 22:23:46 +0600

    chore: Refactor code for better efficiency and readability

--- 1980A.py
+++ 1980A.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 t = int(input())
 
 for _ in range(t):
@@ -7,14 +6,4 @@ for _ in range(t):
     ans = 0
     for i in range(ord('A'),ord('H')):
         ans += max(0, b-s.count(chr(i)))
-=======
-t = int(input())
-
-for _ in range(t):
-    a,b = map(int, input().split())
-    s = input()
-    ans = 0
-    for i in range(ord('A'),ord('H')):
-        ans += max(0, b-s.count(chr(i)))
->>>>>>> a1173defe16d2ead6cc7b1642d086ba68eeae209
     print(ans)
