dfd68beef6cf2e74367887ba09c394f22ccca51b
AbrarZaved <abrarzaved2002@gmail.com>
2024-10-19 19:30:15 +0600

    removed the comments

--- 1800A Is It a Cat .py
+++ 1800A Is It a Cat .py
@@ -1,18 +1,16 @@
 def solve():
-    n = int(input())  # Read integer input
-    s = input()  # Read the string input
-    s = s.lower()  # Convert string to lowercase
-    # Remove consecutive duplicates
+    n = int(input()) 
+    s = input() 
+    s = s.lower() 
     result = []
     for i in range(n):
         if i == 0 or s[i] != s[i - 1]:
             result.append(s[i])
-    # Join the list to form a new string without consecutive duplicates
     result = "".join(result)
     print("YES" if result == "meow" else "NO")
 
 
-# Main function to handle multiple test cases
-t = int(input())  # Read the number of test cases
+
+t = int(input())
 for _ in range(t):
     solve()
